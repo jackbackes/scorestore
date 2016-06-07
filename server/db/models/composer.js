e8c7ad7e5a3986b1b5f3117a8b22d6e92dd2b4cf
thejohnbackes <jbackes@thisismotive.com>
2016-06-07 13:31:46 -0400

    cleaned up some composer.js

--- server/db/models/composer.js
+++ server/db/models/composer.js
@@ -2,42 +2,43 @@
 
 
 var Sequelize = require('sequelize');
-var database = require('./_db');
+var database = require('../_db');
 var Genre = require('./genre')(database);
 
-module.exports = function (db) {
+module.exports = function(db) {
 
-    return db.define('composer', {
-        firstName: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        lastName: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        fullName: {
-            type: Sequelize.VIRTUAL,
-            get: function () {
-              return this.firstName + " " + this.lastName;
-            }
-        }
-    }, {
-        instanceMethods: {
-            findSimilar: function () {
-              return this.Model.findAll({
-                include: [{
-                  model: Genre,
-                  through: {
-                    where: {
-                      genreId: this.genreId,
-                      composerId: {
-                        $ne: this.id
-                      }
+        return db.define('composer', {
+                firstName: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                lastName: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                fullName: {
+                    type: Sequelize.VIRTUAL,
+                    get: function() {
+                        return this.firstName + " " + this.lastName;
                     }
-                  }
-                }]
-            });
-        },
-    });
-};
+                }
+            }, {
+                instanceMethods: {
+                    findSimilar: function() {
+                        return this.Model.findAll({
+                            include: [{
+                                model: Genre,
+                                through: {
+                                    where: {
+                                        genreId: this.genreId,
+                                        composerId: {
+                                            $ne: this.id
+                                        }
+                                    }
+                                }
+                            }]
+                        });
+                      }
+                    },
+                });
+        };
