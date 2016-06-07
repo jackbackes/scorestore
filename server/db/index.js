8324fdccd5cc5f4bef78fc59ebe41cadf8289697
thejohnbackes <jbackes@thisismotive.com>
2016-06-06 22:07:38 -0400

    added order-song index table with quantities.

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,26 @@
 'use strict';
-var db = require('./_db');
-var User = require('./models/user')(db);
-var Song = require('./models/song')(db);
-// var Composer = require('./models/composer')(db)
+const db = require('./_db');
+const User = require('./models/user')(db);
+const Song = require('./models/song')(db);
+const Address = require('./models/address')(db);
+const Review = require('./models/review')(db);
+const Composer = require('./models/composer')(db)
+const OrderSong = require('./models/orderSong')(db)
 
 
-// Song.belongsTo(Composer);
-// Song.belongsTo(Genre);
-// Song.belongsToMany(Order, {through: 'song_order'});
-// Review.belongsTo(Song);
 
-var User = require('./models/user')(db);
-var Address = require('./models/address')(db);
-var Review = require('./models/review')(db);
-module.exports = db;
+Song.belongsTo(Composer);
+Song.belongsTo(Genre);
+Order.hasMany(Song, {through: 'song_order'});
+Review.belongsTo(Song);
 
 User.hasMany(Address);
 User.hasMany(Review);
 Review.belongsTo(User);
+
+
+module.exports = db;
+
+
+// const models = require('./models')
+// const User = models.User;
