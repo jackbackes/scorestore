08abac910a151998583af33de4f9f3df9e3e1431
Geoff Bass <geoff.c.bass@gmail.com>
2016-06-09 18:23:30 -0400

    Basic song routes set up

---
+++ server/app/routes/songs/index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const path = require('path');
+const router = require('express').Router();
+const db = require(path.join(__dirname, '../../../db'));
+const Song = db.model('song');
+const Composer = db.model('composer');
+const Genre = db.model('genre');
+const Photo = db.model('photo');
+
+router.get('/', function (req, res, next){
+  return Song.findAll({
+    include: [Composer, Genre, Photo],
+    where: req.query
+  })
+  .then(function (songs) {
+    res.send(songs);
+  })
+  .catch(next);
+});
+
+router.param('id', function (req, res, next, id) {
+  return Song.findById(id, {include: [Composer, Genre, Photo]})
+  .then(function (song) {
+    req.song = song;
+    next();
+  })
+  .catch(next);
+});
+
+router.post('/', function (req, res, next) {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      Song.create(req.body)
+      .then(function () {
+        res.sendStatus(201);
+      })
+      .catch(next);
+    } else {
+      res.sendStatus(403);
+    }
+   } else {
+    res.sendStatus(401);
+  }
+});
+
+router.get('/:id', function (req, res, next) {
+  res.send(req.song);
+});
+
+router.delete('/:id', function (req, res, next) {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      req.song.destroy()
+      .then(function () {
+        res.sendStatus(204);
+      })
+      .catch(next);
+    } else {
+      res.sendStatus(403);
+    }
+   } else {
+    res.sendStatus(401);
+  }
+});
+
+router.put('/:id', function (req, res, next) {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      req.song.update(req.body)
+      .then(function () {
+        res.sendStatus(200);
+      })
+      .catch(next);
+    } else {
+      res.sendStatus(403);
+    }
+   } else {
+    res.sendStatus(401);
+  }
+});
+
+module.exports = router;
