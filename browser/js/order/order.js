7dd7788f77aa22603868d7f2805924a6354d44dd
Jon Perrelle <jonperrelle@gmail.com>
2016-06-11 15:31:21 -0400

    integrating stripe

--- browser/js/order/order.js
+++ browser/js/order/order.js
@@ -25,4 +25,28 @@ app.controller('OrderCtrl', function ($scope, Session, $state, OrderFactory, Car
 		return CartFactory.getCartTotal();
 	};
 
+	$scope.submitPayment = function(status, response) {
+		console.log("status", status);
+		console.log('response', response);
+		// console.log(billingInfo)
+		// handler.open({
+	 //      name: 'Stripe.com',
+	 //      description: '2 widgets',
+	 //      amount: 2000
+  //   	});
+	};
+
+
+
+	// var handler = StripeCheckout.configure({
+	//     key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+	//     image: '/img/documentation/checkout/marketplace.png',
+	//     locale: 'auto',
+	//     token: function(token) {
+	//     	console.log("here", token)
+	//       // You can access the token ID with `token.id`.
+	//       // Get the token ID to your server-side code for use.
+	//     }
+	//   });
+
 })
