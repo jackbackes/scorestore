dd90a79cfd61be40b797f124de156985138d9e09
thejohnbackes <jbackes@thisismotive.com>
2016-06-11 16:41:50 -0400

    Update development.js

--- server/env/development.js
+++ server/env/development.js
@@ -1,19 +1,3 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fsg",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
-  },
-  "FACEBOOK": {
-    "clientID": "1431968783496727",
-    "clientSecret": "2ecaf8a1ad0f4e21b397dd58be75c303",
-    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
-  },
-  "GOOGLE": {
-    "clientID": "422783382244-3v2ue47880kjunhkh96uj3i4jvuni14l.apps.googleusercontent.com",
-    "clientSecret": "6zcgLsBFZPuLo8wd1vPPnC5k",
-    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
-  }
+// keys removed. check evernote file that was shared to team.
 };
